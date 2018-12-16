d122a307fbe02ca590b44496cc9142ae7ba20ad3
Iurii Prytula <iurii.prytula@gmail.com>
2018-12-16 12:23:10 +0100

    gulp srtarter for looplab

---
+++ looplab/gulp-tasks/styles.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const $ = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const multipipe = require('multipipe');
+
+var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+module.exports = function(options) {
+  return function() {
+    return multipipe(
+      gulp.src(options.src, {since: gulp.lastRun(options.since)}),
+      $.if(isDevelopment, $.sourcemaps.init()),
+      $.sass().on('error', $.sass.logError),
+      $.autoprefixer(),
+      $.csso(),
+      $.sourcemaps.write('.'),
+      gulp.dest('public')
+    ).on('error', $.notify.onError());
+  };
+};
