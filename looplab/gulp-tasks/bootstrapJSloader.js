e8ebcd56f5e055dd31754c8f69ce5794b4ee3edb
Iurii Prytula <iurii.prytula@gmail.com>
2018-12-17 16:35:48 +0100

    create navigation

--- looplab/gulp-tasks/bootstrapJSloader.js
+++ looplab/gulp-tasks/bootstrapJSloader.js
@@ -4,17 +4,11 @@ const $ = require('gulp-load-plugins')();
 const gulp = require('gulp');
 const multipipe = require('multipipe');
 
-var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
-
 module.exports = function(options) {
   return function() {
     return multipipe(
       gulp.src(options.src, {since: gulp.lastRun(options.since)}),
-      $.if(isDevelopment, $.sourcemaps.init()),
-      $.sass().on('error', $.sass.logError),
-      $.autoprefixer(),
-      $.csso(),
-      $.sourcemaps.write('.'),
+      $.newer(options.dst),
       gulp.dest(options.dst)
     ).on('error', $.notify.onError());
   };
--- looplab/gulpfile.js
+++ looplab/gulpfile.js
@@ -13,7 +13,8 @@ function lazyRequireTask(taskName, path, options) {
 
 lazyRequireTask('styles', './gulp-tasks/styles.js', {
   src: 'frontend/styles/main.scss',
-  since: 'styles'
+  since: 'styles',
+  dst: 'public/css'
 });
 
 lazyRequireTask('pug', './gulp-tasks/pug.js', {
@@ -34,7 +35,7 @@ lazyRequireTask('images', './gulp-tasks/images.js', {
 });
 
 lazyRequireTask('js', './gulp-tasks/js.js', {
-  src: 'frontend/js/**/*.*',
+  src: 'frontend/js/**/*.js',
   since: 'js',
   dst: 'public/js'
 });
@@ -42,14 +43,38 @@ lazyRequireTask('js', './gulp-tasks/js.js', {
 gulp.task('build:all', gulp.series('clean', gulp.parallel('pug', 'styles','js' ,'images')));
 
 gulp.task('watch', function() {
-  gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
+  gulp.watch('frontend/styles/**/*.scss', gulp.series('styles'));
   gulp.watch('frontend/assets/img/**/*.*', gulp.series('images'));
-  gulp.watch('frontend/assets/js/**/*.*', gulp.series('js'));
-  gulp.watch('frontend/assets/pages/**/*.pug', gulp.series('pug'));
+  gulp.watch('frontend/pages/**/*.pug', gulp.series('pug'));
 });
 
 lazyRequireTask('serve', './gulp-tasks/serve.js', {
   server: 'public'
 });
 
-gulp.task('dev', gulp.series('build:all', gulp.parallel('watch', 'serve')));
+lazyRequireTask('bootstrap:js', './gulp-tasks/bootstrapJSloader.js', {
+  src: './node_modules/bootstrap/dist/js/bootstrap.min.js',
+  since: 'bootstrap:js',
+  dst: 'public/js/'
+});
+
+lazyRequireTask('bootstrap:css', './gulp-tasks/bootstrapCSSloader.js', {
+  src: './node_modules/bootstrap/dist/css/bootstrap.min.css',
+  since: 'bootstrap:css',
+  dst: 'public/js/'
+});
+
+lazyRequireTask('jquery', './gulp-tasks/jqueryLoader.js', {
+  src: './node_modules/jquery/dist/jquery.min.js',
+  since: 'jquery',
+  dst: 'public/js/'
+});
+
+lazyRequireTask('popper', './gulp-tasks/popperLoader.js', {
+  src: './node_modules/popper.js/dist/popper.min.js',
+  since: 'popper',
+  dst: 'public/js/'
+});
+
+gulp.task('load:modules', gulp.series('bootstrap:js', 'bootstrap:css', 'popper', 'jquery'));
+gulp.task('dev', gulp.series('build:all', 'load:modules', gulp.parallel('watch', 'serve')));
